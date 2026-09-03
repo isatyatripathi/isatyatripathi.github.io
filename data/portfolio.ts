f2192200928f3d57284e1a020954c22331cd9637
Satya Tripathi <168341486+isatyatripathi@users.noreply.github.com>
2026-09-04 02:31:54 +0530

    Remove publication link from portfolio
    Removed link to IEEE publication from portfolio.

--- data/portfolio.ts
+++ data/portfolio.ts
@@ -349,7 +349,6 @@ export const portfolio: Portfolio = {
   publication: {
     title: "Study on Secured Framework for Cloud-Based Online Banking",
     publisher: "IEEE publication",
-    link: "https://ieeexplore.ieee.org/document/8229915"
   },
   resume: {
     path: "/resume/Satya-Tripathi-Resume.pdf",
